e956429c949118576947da7e0071cff20a25db80
tomlottermann <tomlottermann@307f314c-bf8c-a4ea-f323-c3ccb8fc0081>
2010-09-30 01:29:04 +0000

    Added CallerListener

---
+++ epursuit/src/org/lotterm/asterisk/epursuit/caller/CallerListener.java
@@ -0,0 +1,7 @@
+package org.lotterm.asterisk.epursuit.caller;
+
+public interface CallerListener {
+	public void mrxCallsFinished();
+	public void testCallFinished();
+	public void agentCallsFinished();
+}
